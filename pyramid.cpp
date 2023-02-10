400536f6f4d982d0da3002f4663b82d9bd1de9a2
Harshanaruchami <harshanaruchami@gmail.com>
2023-02-10 09:46:58 +0530

    pyramid

--- pyramid.cpp
+++ pyramid.cpp
@@ -2,16 +2,22 @@
 using namespace std;
 int main()
 {
-    int i,j,n;
+    int i,j,n,k,l;
     cout<<"Enter the no:of rows needed";
     cin>>n;
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j--)
+        for(j=n;j>i;j--)
         {
-            cout<<j<<"   ";
+            cout<<"   ";
 
         }
-        cout<<i;
+        for(k=1;k<=i;k++){
+            cout<<k<<"  ";
+        }
+        for(l=i-1;l>=1;l--){
+            cout<<l<<"  ";
+        }
+        cout<<endl;
     }
 }
