a8383836cbb01f60302d751c4af2e7e8ebff0900
Harshanaruchami <harshanaruchami@gmail.com>
2023-02-13 16:45:21 +0530

    prime commit

--- primearr.cpp
+++ primearr.cpp
@@ -3,13 +3,8 @@ using namespace std;
 int main()
 {
     int arr[20];
-    int evenar[10];
-    int oddar[10];
-    int e=0;
-    int o=0;
-    int b,i,j;
+    int b,i,j,pr;
     cout<<"************To find the PRIME NUMBERS in a ARRAY **********"<<endl;
-
     cout<<"Enter the total number of elements you want : "<< endl;
     cin>>b;
     for( i=0; i<b; i++)
@@ -17,8 +12,23 @@ int main()
         cout<<"Enter the number "<<i<<endl;
         cin>>arr[i];
         }
-
-    cout<<"The array elements are \n";
-    for (int i=0;i<b;i++){
-      cout<<arr[i]<<"   \t";}
+cout<<"The array elements are \n";
+for (int i=0;i<b;i++){
+      cout<<arr[i]<<"   \t"<<endl;}
+      //to print the prime numbers
+    for(int i=0;i<b;i++)
+    {
+    pr=0;
+for(int j=2;j<=arr[i]/2;j++)
+{
+if(arr[i]%j==0)
+{
+pr=1;
 }
+}
+ if(pr==0){
+          cout<<arr[i]<<"  ";}
+}
+cout<<"  are the PRIME NUMBERS In the array are"<<endl;
+}
+
