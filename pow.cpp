3171adbc32cede02b72388b6b0b36474b716bf8a
Harshanaruchami <harshanaruchami@gmail.com>
2023-02-18 16:59:31 +0530

    fgdf

---
+++ pow.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+  a,b,result;
+ cout<<"Enter the 1st number : ";
+ cin>>a;
+ cout<<"Enter the 2nd number : ";
+ cin>>b;
+ cout<<"The power of "<<a<<" ^ "<<b<<"is: "<<pow(a,b)<<endl;
+}
