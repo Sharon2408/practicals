9d78fded1610069654b7c8979b75cf423bdac955
Harshanaruchami <harshanaruchami@gmail.com>
2023-02-23 16:49:57 +0530

    fg

---
+++ form.js
@@ -0,0 +1,6 @@
+function sub()
+{
+ const form= document.regForm.name.value;
+ const email=form.regForm.email.value;
+ document.writeln(email)
+}
--- function.js
+++ function.js
@@ -7,70 +7,64 @@ function prompter(num) {
         }
     }
     if (sum == num) {
-        alert("It is a perfect number")
+        document.write("It is a perfect number")
     }
     else {
-        alert("It is not a perfect number")
+        document.writeln("It is not a perfect number")
     }
 }
 function factor(fact) {
     var numb = document.getElementById('fact').value;
-     i = 1;
+    i = 1;
     f = 1;
     do {
-        f=f*i;
+        f = f * i;
         i++;
     }
-    while (i<=numb) ;
-    alert(f)
+    while (i <= numb);
+    document.writeln(f)
 
 
 }
-function fibonac(fib)
-{
-let t1=0;
-let t2=1;
-let t3;
-var numa = document.getElementById('fib').value;
-while(numa>=0){
-t3=t1+t2;
-t1=t2;
-t2=t3;
-document.write(t3+"</br></br>")
-numa--;
-}
-}
-function gender()
-{
-    var gend = document.getElementById('gend').value; 
-    if(gend=='male' || gend=='MALE')
-    {
-        alert("You are male")
-    } 
-    else if (gend=='female' || gend=='FEMALE'){
-        alert("You are Female")
-    }
-    else
-    {
-        alert("Invalid Gender")
+function fibonac(fib) {
+    let t1 = 0;
+    let t2 = 1;
+    let t3;
+    var numa = document.getElementById('fib').value;
+    while (numa >= 0) {
+        t3 = t1 + t2;
+        t1 = t2;
+        t2 = t3;
+        document.writeln(t3 + "</br></br>")
+        numa--;
     }
 }
-function genderswitch()
-{
-    var gender = document.getElementById('gend').value; 
-    switch(gender)
-{
-    case 'male' :
-        alert("You are male")
-        break;
-        case 'MALE' :
-            alert("You are male")
-            break;
-            case 'female' :
-            alert("You are female")
-            break;
-            case 'FEMALE' :
-            alert("You are female")
-            break;
+function gender() {
+    var gend = document.getElementById('gend').value;
+    if (gend == 'male' || gend == 'MALE') {
+        document.writeln("You are male")
+    }
+    else if (gend == 'female' || gend == 'FEMALE') {
+        document.writeln("You are Female")
+    }
+    else {
+        document.writeln("Invalid Gender")
+    }
 }
+function genderswitch() {
+    var gender = document.getElementById('gend').value;
+    switch (gender) {
+        case 'male':
+            document.writeln("You are male")
+            break;
+        case 'MALE':
+            document.writeln("You are male")
+            break;
+        case 'female':
+            document.writeln("You are female")
+            break;
+        case 'FEMALE':
+            document.writeln("You are female")
+            break;
+    }
 }
