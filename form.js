448ea2577e6246e089fc5254ad3cd3057964f2f3
Harshanaruchami <harshanaruchami@gmail.com>
2023-02-23 16:50:23 +0530

    s

--- form.js
+++ form.js
@@ -4,3 +4,22 @@ function sub()
  const email=form.regForm.email.value;
  document.writeln(email)
 }
+function sub() {
+    var a=document.regform.email.value;
+    var b=document.regform.degree.value;
+    var c=document.regform.Gender.value;
+    var d=document.regform.address.value;
+    var e=document.regform.dob.value;
+    var f=document.regform.Profession.value;
+    var g=document.regform.mnumber.value;
+    var h=document.regform.tnumber.value;
+    document.write(a +"</br>");
+    document.write(b +"</br>");
+    document.write(c +"</br>");
+    document.write(d +"</br>");
+    document.write(e +"</br>");
+    document.write(f +"</br>");
+    document.write(g +"</br>");
+    document.write(h +"</br>");
+    
+}
