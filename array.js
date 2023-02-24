c38b8a1611a92ad18a5f01ae40f58a9e09c0d1cd
Harshanaruchami <harshanaruchami@gmail.com>
2023-02-24 17:05:24 +0530

    dd

---
+++ array.js
@@ -0,0 +1,9 @@
+const friends=["sharon","hari","harshan","hemanth"];
+
+function add_begining()
+{
+    value1 = document.getElementById("friend").innerHTML = friends;
+     friends.push(value1);
+     document.write(friends + "</br>");
+    
+}
